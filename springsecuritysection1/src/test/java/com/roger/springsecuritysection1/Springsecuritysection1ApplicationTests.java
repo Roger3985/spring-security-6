c86da89ba624ecc71f713f1f371d690a74c4a782
zhengchangsun <a0967067696@gmail.com>
2024-07-30 14:21:31 +0800

    practice on spring-security-6

---
+++ springsecuritysection1/src/test/java/com/roger/springsecuritysection1/Springsecuritysection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roger.springsecuritysection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecuritysection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
