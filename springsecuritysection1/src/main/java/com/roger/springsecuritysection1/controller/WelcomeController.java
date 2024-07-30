e1bdd4523e1977ed2d469e938dfb73cd7d3323cd
zhengchangsun <a0967067696@gmail.com>
2024-07-30 14:31:22 +0800

    new practice on security

---
+++ springsecuritysection1/src/main/java/com/roger/springsecuritysection1/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.roger.springsecuritysection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 2407009
+ * @Description
+ * @create 2024-07-30
+ */
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring Application with out security.";
+    }
+}
